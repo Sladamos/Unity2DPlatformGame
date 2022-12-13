cf834c06c378a525770759a17d4c51ef2a9ebaab
pikol93 <pikol93@wp.pl>
2022-12-13 20:44:53 +0100

    Added passed time to UI

---
+++ Assets/Scripts/GameManagers/TimeDisplayer.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+
+namespace MIIProjekt.GameManagers
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TimeDisplayer : MonoBehaviour
+    {
+        private const int SECONDS_IN_MINUTE = 60;
+        private const int MILLISECONDS_IN_SECOND = 1000;
+        private const int MILLISECONDS_IN_MINUTE = SECONDS_IN_MINUTE * MILLISECONDS_IN_SECOND;
+
+        [SerializeField]
+        private TimeManager timeManager;
+
+        private TextMeshProUGUI text;
+
+        private void UpdateDisplayedTime(float secondsPassed)
+        {
+            if (text != null)
+            {
+                int millisecondsPassed = (int)(secondsPassed * 1000.0f);
+                int minutes = millisecondsPassed / MILLISECONDS_IN_MINUTE;
+                int seconds = (millisecondsPassed / MILLISECONDS_IN_SECOND) % SECONDS_IN_MINUTE;
+                int milliseconds = millisecondsPassed % MILLISECONDS_IN_SECOND;
+                text.SetText(string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds));
+            }
+        }
+
+        private void Awake()
+        {
+            text = GetComponent<TextMeshProUGUI>();
+            if (timeManager != null) {
+                timeManager.TimeUpdated += UpdateDisplayedTime;
+            }
+        }
+    }
+}
--- Assets/Scripts/GameManagers/TimeManager.cs
+++ Assets/Scripts/GameManagers/TimeManager.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace MIIProjekt.GameManagers
 {
     public class TimeManager : MonoBehaviour
     {
+        [SerializeField]
+        public event Action<float> TimeUpdated;
+
+        public float TimePassed { get; private set; }
+
+        private void Update()
+        {
+            TimePassed += Time.deltaTime;
+            TimeUpdated?.Invoke(TimePassed);
+        }
+
         private void UpdateTime()
         {
             if (GameManager.instance.IsGameCurrentlyPlayed())
