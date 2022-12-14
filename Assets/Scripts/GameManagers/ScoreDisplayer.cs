469a6744b98da34aff66069372df8a149ebb88fe
Sladamos <sladamo@wp.pl>
2022-12-14 11:35:29 +0100

    ready to merge

--- Assets/Scripts/GameManagers/ScoreDisplayer.cs
+++ Assets/Scripts/GameManagers/ScoreDisplayer.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MIIProjekt.GameManagers
 {
@@ -11,10 +12,39 @@ namespace MIIProjekt.GameManagers
         [SerializeField]
         private TMP_Text endScoreText;
 
+        [SerializeField]
+        private TMP_Text highScoreText;
+
+        private string keyHighScore = "HighScoreLevel1";
+
+        private void Awake()
+        {
+            if(!PlayerPrefs.HasKey(keyHighScore))
+            {
+                PlayerPrefs.SetInt(keyHighScore, 0);
+            }
+        }
+
         private void SetScore(int score)
         {
             scoreText.text = score.ToString("000");
             endScoreText.text = "Your score: " + score.ToString("0000");
+            CheckHighestScore(score);
+        }
+
+        private void CheckHighestScore(int score)
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            if(currentScene.name == "Level1")
+            {
+                int highScore = PlayerPrefs.GetInt(keyHighScore);
+                if(highScore < score)
+                {
+                    highScore = score;
+                    PlayerPrefs.SetInt(keyHighScore, score);
+                }
+                highScoreText.text = "Highest score: " + highScore.ToString("0000");
+            }
         }
     }
 }
