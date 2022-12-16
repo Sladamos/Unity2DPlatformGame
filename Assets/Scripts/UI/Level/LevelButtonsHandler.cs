65ae13693f118f39d95ddda4d691903670252344
Sladamos <sladamo@wp.pl>
2022-12-17 00:40:40 +0100

    Ready for inst. 6

---
+++ Assets/Scripts/UI/Level/LevelButtonsHandler.cs
@@ -0,0 +1,24 @@
+﻿using MIIProjekt.GameManagers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MIIProjekt.UI.Level
+{
+    public class LevelButtonsHandler : MonoBehaviour
+    {
+        public void OnClickedButtonResume()
+        {
+            GameManager.instance.InGame();
+        }
+
+        public void OnClickedButtonRestart()
+        {
+            SceneManager.LoadScene((SceneManager.GetActiveScene().name));
+        }
+
+        public void OnClickedButtonMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
