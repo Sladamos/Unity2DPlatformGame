f53401d7d7c18d4eb16f98fdbc3f8130d649f85b
pikol93 <pikol93@wp.pl>
2022-12-20 16:41:33 +0100

    Modified the VerifyNotNull method to throw different exception

--- Assets/Scripts/Extensions/ObjectExtensions.cs
+++ Assets/Scripts/Extensions/ObjectExtensions.cs
@@ -5,16 +5,35 @@ namespace MIIProjekt.Extensions
     public static class ObjectExtensions
     {
         /// <summary>
-        /// Verifies the object is not null. If the object is null, a NullPointerException is thrown. 
+        /// Verifies the object is not null. If the object is null, an ArgumentNullException is thrown. 
         /// </summary>
         /// <param name="obj">Object to check.</param>
         /// <returns>Object that the method is executed on.</returns>
-        /// <exception cref="NullReferenceException">If the passed argument is null.</exception>
+        /// <exception cref="ArgumentNullException">If the passed argument is null.</exception>
         public static T VerifyNotNull<T>(this T obj)
+        {
+            return VerifyNotNull(obj, String.Empty);
+        }
+
+        /// <summary>
+        /// Verifies the object is not null. If the object is null, a ArgumentNullException is thrown. 
+        /// </summary>
+        /// <param name="obj">Object to check.</param>
+        /// <param name="message">Message passed to the ArgumentNullException ctor that is called if the object is null.</message>
+        /// <returns>Object that the method is executed on.</returns>
+        /// <exception cref="ArgumentNullException">If the passed argument is null.</exception>
+        public static T VerifyNotNull<T>(this T obj, string message)
         {
             if (obj == null)
             {
-                throw new NullReferenceException();
+                if (String.IsNullOrEmpty(message))
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                {
+                    throw new ArgumentNullException(message);
+                }
             }
 
             return obj;
