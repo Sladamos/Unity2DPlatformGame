58278eb8969be32bbd1ad9bc2289aa2c98e39e0f
pikol93 <pikol93@wp.pl>
2022-12-20 16:58:46 +0100

    Moved TimeDisplayer

---
+++ Assets/Scripts/UI/Level/TimeDisplayer.cs
@@ -0,0 +1,45 @@
+﻿using MIIProjekt.GameManagers;
+using MIIProjekt.Logging;
+using NLog;
+using TMPro;
+using UnityEngine;
+
+namespace MIIProjekt.UI.Level
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TimeDisplayer : MonoBehaviour
+    {
+        private const int SECONDS_IN_MINUTE = 60;
+
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private TMP_Text timeText;
+
+        [SerializeField]
+        private TimeManager timeManager;
+
+        private void UpdateDisplayedTime(float secondsPassed)
+        {
+            int minutes = (int) (secondsPassed / SECONDS_IN_MINUTE);
+            int seconds = (int) (secondsPassed % SECONDS_IN_MINUTE);
+
+            timeText.SetText(string.Format("{0:00}:{1:00}", minutes, seconds));
+        }
+
+        private void Awake()
+        {
+            LoggingManager.InitializeLogging();
+
+            timeText = GetComponent<TMP_Text>();
+            
+            if (timeManager != null)
+            {
+                timeManager.TimeUpdated += UpdateDisplayedTime;
+            }
+            else
+            {
+                Logger.Warn("An instance of TimeDisplayer does not have an assigned TimeManager instance. Displayed time will not be updated. Current instance name = {}", name);
+            }
+        }
+    }
+}
