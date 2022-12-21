567fe72b005329b036bbec80165891009ef0194e
pikol93 <pikol93@wp.pl>
2022-12-21 11:50:40 +0100

    Moved handling pause menu buttons to PauseMenuComponent

--- Assets/Scripts/UI/Level/PauseMenuComponent.cs
+++ Assets/Scripts/UI/Level/PauseMenuComponent.cs
@@ -1,24 +1,36 @@
-﻿using System;
+using System;
+using MIIProjekt.Logging;
+using NLog;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace MIIProjekt.UI.Level
 {
-    public class LevelButtonsHandler : MonoBehaviour
+    public class PauseMenuComponent : MonoBehaviour
     {
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
         public void OnClickedButtonResume()
         {
+            Logger.Debug("Clicked button resume");
             throw new NotImplementedException();
         }
 
         public void OnClickedButtonRestart()
         {
+            Logger.Debug("Clicked button restart");
             SceneManager.LoadScene((SceneManager.GetActiveScene().name));
         }
 
         public void OnClickedButtonMainMenu()
         {
+            Logger.Debug("Clicked button main menu");
             SceneManager.LoadScene("MainMenu");
+        }
+
+        private void Awake()
+        {
+            LoggingManager.InitializeLogging();
         }
     }
 }
