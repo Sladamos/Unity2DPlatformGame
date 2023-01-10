52ed85451474b321c919b764173162a1569bc835
pikol93 <pikol93@wp.pl>
2023-01-10 19:21:29 +0100

    Added an animation to invoking door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -10,15 +10,68 @@ namespace MIIProjekt
     {
         private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
 
+        private List<ICollectable> Collectables { get; } = new();
+
+        private float invokeTimestamp = 0.0f;
+
+        [Header("CollectableSettings")]
+        [SerializeField]
+        private float collectableDelayPerSecond = 0.4f;
+
+        [SerializeField]
+        private float collectableAcceleration = 2.0f;
+
+        [SerializeField]
+        private float collectableContactDistance = 1.0f;
+
+        public void InvokeCollectableTarget(List<ICollectable> collectables)
+        {
+            this.Collectables.AddRange(collectables);
+            invokeTimestamp = Time.time;
+        }
+
+        private void OnKeyArrived(ICollectable collectable)
+        {
+            collectable.Active = false;
+            Collectables.Remove(collectable);
+            Logger.Trace("Key {} arrived at door {}", collectable, this);
+
+            if (Collectables.Count == 0)
+            {
+                DisableDoor();
+            }
+        }
+
+        private void DisableDoor()
+        {
+            Logger.Info("All keys collected. Opening door...");
+            gameObject.SetActive(false);
+        }
+
         private void Awake()
         {
             LoggingManager.InitializeLogging();
         }
 
-        public void InvokeCollectableTarget(List<ICollectable> collectables)
+        private void Update()
         {
-            Logger.Info("All keys collected. Opening door...");
-            gameObject.SetActive(false);
+            for (int i = 0; i < Collectables.Count; i++)
+            {
+                float timeSinceInvoke = Time.time - invokeTimestamp;
+                float velocityScalar = Mathf.Max(0.0f, timeSinceInvoke - (i * collectableDelayPerSecond)) * collectableAcceleration; 
+                Vector2 difference = transform.position - Collectables[i].Transform.position;
+                float distanceLeft = difference.magnitude;
+                float deltaTimeVelocityScalar = velocityScalar * Time.deltaTime;
+
+                if (distanceLeft - collectableContactDistance < deltaTimeVelocityScalar)
+                {
+                    OnKeyArrived(Collectables[i]);
+                }
+                else
+                {
+                    Collectables[i].Transform.position += (Vector3)(difference.normalized * deltaTimeVelocityScalar);
+                }
+            }
         }
     }
 }
