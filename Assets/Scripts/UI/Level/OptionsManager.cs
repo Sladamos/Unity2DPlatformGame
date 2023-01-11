21756d1a0221acd2d31e479ff475420a0296b1e2
Sladamos <sladamo@wp.pl>
2023-01-11 13:50:23 +0100

    It's possible to change music volume

--- Assets/Scripts/UI/Level/OptionsManager.cs
+++ Assets/Scripts/UI/Level/OptionsManager.cs
@@ -14,15 +14,33 @@ namespace MIIProjekt.UI.Level
         private UIAnimatorController uiAnimatorController;
 
         [SerializeField]
-        private Slider slider;
+        private Slider sliderMaster;
+
+        [SerializeField]
+        private Slider sliderMusic;
 
         [SerializeField]
         private TMP_Dropdown dropdown;
+
+        [SerializeField]
+        private AudioSource music;
 
         public void OnValueChangedMasterVolume(float value)
         {
             Logger.Debug("Master volume set new value: {}", value);
             AudioListener.volume = Mathf.Clamp(value, 0.0f, 1.0f);
+        }
+        public void OnValueChangedMusicVolume(float value)
+        {
+            if (music != null)
+            {
+                Logger.Debug("Background music volume set new value: {}", value);
+                music.volume = Mathf.Clamp(value, 0.0f, 1.0f);
+            }
+            else
+            {
+                Logger.Debug("Background music is not set!");
+            }
         }
 
         public void OnValueChangedGraphics(int value)
@@ -46,18 +64,27 @@ namespace MIIProjekt.UI.Level
                 Logger.Warn("UIAnimatorController is not set on OptionsManager instance. GameObject name = {}", name);
             }
 
-            if (slider != null)
+            if (sliderMaster != null)
             {
-                slider.value = AudioListener.volume;
+                sliderMaster.value = AudioListener.volume;
             }
             else
             {
                 Logger.Warn("Slider component is not set on the OptionsManager instance. The default value will not be applied to the slider.");
             }
 
+            if (sliderMusic != null && music != null )
+            {
+                sliderMusic.value = music.volume;
+            }
+            else
+            {
+                Logger.Warn("It's problem with slider or backgroudnMusic component");
+            }
+
             if (dropdown != null)
             {
-                slider.value = QualitySettings.GetQualityLevel();
+                dropdown.value = QualitySettings.GetQualityLevel();
             }
             else
             {
