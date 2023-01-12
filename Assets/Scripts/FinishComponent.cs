46c9af73cedfeda0096932a021591c704309ff00
Sladamos <sladamo@wp.pl>
2023-01-12 20:44:39 +0100

    Bugfix end game

--- Assets/Scripts/FinishComponent.cs
+++ Assets/Scripts/FinishComponent.cs
@@ -1,6 +1,7 @@
 ﻿using MIIProjekt.Collectables;
 using MIIProjekt.GameManagers;
 using MIIProjekt.Logging;
+using MIIProjekt.Player;
 using NLog;
 using UnityEngine;
 
@@ -13,6 +14,9 @@ namespace MIIProjekt
         [SerializeField]
         private LevelManager levelManager;
 
+        [SerializeField]
+        private PlayerController2 player;
+
         private void CollectableArrived(ICollectable collectable)
         {
             Logger.Info("Collectable!");
@@ -21,6 +25,7 @@ namespace MIIProjekt
         private void AllCollectablesArrived()
         {
             Logger.Info("Zebrano wszystkie klucze");
+            player.SendMessage("Finish");
             levelManager.InvokeLevelCompleted();
         }
 
