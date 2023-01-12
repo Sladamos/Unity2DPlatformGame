aab3c983d4c941aad05ce6d64fa44378f5f503b6
Sladamos <sladamo@wp.pl>
2023-01-12 14:44:59 +0100

    Level won music

--- Assets/Scripts/UI/Level/States/GameUIStateLevelCompleted.cs
+++ Assets/Scripts/UI/Level/States/GameUIStateLevelCompleted.cs
@@ -12,6 +12,7 @@ namespace MIIProjekt.UI.Level.States
         {
             Logger.Trace("UI state LevelCompleted entered");
             var gameObject = animator.GetComponent<UIManager>()?.UIObjectLevelCompleted;
+            SoundsManager soundsManager = animator.GetComponent<SoundsManager>();
             if (gameObject != null)
             {
                 gameObject.SetActive(true);
@@ -20,6 +21,7 @@ namespace MIIProjekt.UI.Level.States
             {
                 Logger.Warn("Cannot set GameOver UI GameObject as active");
             }
+            soundsManager.playLevelCompletedSound();
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
---
+++ Assets/Scripts/UI/SoundsManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MIIProjekt.UI
+{
+    public class SoundsManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource levelMusic;
+
+        [SerializeField]
+        private AudioClip backgroundMusic;
+
+        [SerializeField]
+        private AudioClip levelCompletedMusic;
+
+        private float musicChangeTime = 2.0f;
+
+        public void playLevelCompletedSound()
+        {
+            playSound(levelCompletedMusic);
+        }
+
+        public void playGameSound()
+        {
+            playSound(backgroundMusic);
+        }
+
+        private void playSound(AudioClip clip)
+        {
+            if(levelMusic.clip != clip)
+            {
+                levelMusic.clip = clip;
+                levelMusic.Play();
+            }
+        }
+    }
+}
