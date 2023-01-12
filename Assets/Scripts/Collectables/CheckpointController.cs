72e9e0f2ed2b7cb927a268037c4bca57c5e1a027
Sladamos <sladamo@wp.pl>
2023-01-12 10:06:37 +0100

    Checkpoints without animation

---
+++ Assets/Scripts/Collectables/CheckpointController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NLog;
+using UnityEngine;
+
+namespace MIIProjekt.Collectables
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class CheckpointController : MonoBehaviour
+    {
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+        private Collider2D selfCollider;
+
+        private void Awake()
+        {
+            selfCollider = GetComponent<Collider2D>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                Logger.Debug("Checkpoint reached");
+                Vector2 newSpawnPoint = transform.position;
+                collider.SendMessage("SetSpawnPoint", newSpawnPoint);
+                selfCollider.enabled = false;
+            }
+        }
+    }
+}
