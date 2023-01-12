1f282d4b817fe5597c0944b49344aef244e3cad6
Sladamos <sladamo@wp.pl>
2023-01-12 17:32:40 +0100

    Basic displaying of text

---
+++ Assets/Scripts/UI/DrawingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace MIIProjekt.UI
+{
+    public class DrawingManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private TMP_Text highTemperaturesText;
+
+        public void DrawHighTemperaturesText()
+        {
+            DrawText(highTemperaturesText, 1.0f, 3.0f);
+        }
+
+        private void DrawText(TMP_Text textToDraw, float fadeTime, float duration)
+        {
+            textToDraw.gameObject.SetActive(true);
+            Debug.Log("mimimimi");
+            //textToDraw.
+        }
+    }
+}
