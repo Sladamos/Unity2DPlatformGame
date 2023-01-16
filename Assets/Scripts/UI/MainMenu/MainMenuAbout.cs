04a4502fea551734e416513e7e6388cb5dcd46b7
Sladamos <sladamo@wp.pl>
2023-01-16 01:24:13 +0100

    Repaired commit

---
+++ Assets/Scripts/UI/MainMenu/MainMenuAbout.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MIIProjekt.UI.MainMenu
+{
+    public class MainMenuAbout : MonoBehaviour
+    {
+        [SerializeField]
+        private MainMenuComponent mainMenu;
+        public void OnBackButtonClicked()
+        {
+            gameObject.SetActive(false);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+}
